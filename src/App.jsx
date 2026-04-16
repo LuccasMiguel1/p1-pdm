577cb1a378fd4f0bb3cbd8afb6ecaea27920be3a
LuccasMiguel1 <lumi137@hotmail.com>
2026-04-16 19:33:46 -0300

    Feat(p1): Implementa a entrada de dados

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import CapturaDados from './CapturaDados'
 
 
 const App = () => {
  return (
   <div className="container rounded">
-    <h1>Hello, Investimentos</h1>          
+    <CapturaDados />
+
+                 
   </div>
   )
 }
---
+++ src/CapturaDados.jsx
@@ -0,0 +1,36 @@
+import react from "react";
+
+const CapturaDados = () => {
+    return (
+        <div className="container rounded">
+            <h2>Simulador de Investimentos</h2>
+            <p>Descubra quanto seu dinheiro pode render com juros compostos</p>
+            <div className="row">
+                <div className="col-md-6 mb-2">
+                    <p>Valor Inicial (R$)</p>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col-md-6">
+                    <p>Aporte Mensal (R$)</p>
+                    <input type="text" className="form-control"/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 mb-2">
+                    <p>Taxa de juros (% ao mês)</p>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col-md-6">
+                    <p>Período (meses)</p>
+                    <input type="text" className="form-control"/>
+                </div>
+            </div>
+            <button className="col-8 me-5 rounded">Calcular</button>
+            <button className="col-3 rounded">Limpar</button>
+        </div>
+    )
+
+
+}
+
+export default CapturaDados;
